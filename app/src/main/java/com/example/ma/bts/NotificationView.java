136cc7fdd113b66b75f936a406181049ec52a092
Mohammad Taleb <Mohammad.m.taleb@gmail.com>
2018-11-01 22:43:13 +0200

    update

---
+++ app/src/main/java/com/example/ma/bts/NotificationView.java
@@ -0,0 +1,17 @@
+package com.example.ma.bts;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mohammed_Aqrabawi on 10/20/2018.
+ */
+
+public class NotificationView extends AppCompatActivity {
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.notification);
+	}
+}
